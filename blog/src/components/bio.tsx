208e8e05aff7cad7fef47d816392ceaa71cb7833
chiara011 <36208248+chiara011@users.noreply.github.com>
2021-10-18 14:33:02 +0200

    added a bio

--- blog/src/components/bio.tsx
+++ blog/src/components/bio.tsx
@@ -31,10 +31,15 @@ export const Bio = () => {
   return (
     <>
       <h1>Bio</h1>
-      <p>Hello this is the bio.</p>
-      <p>Hello this is the bio.</p>
-      <p>Hello this is the bio.</p>
-      <p>Hello this is the bio.</p>
+      <p>I'm Chiara Bianchimani.</p>
+      <p> I'm currently doing a Bachelor Degree in Computer Engineering at 
+        PoliMi (Polytechnic University of Milan). My main topics of interest are: 
+        <li className="bio-list">Game Design</li>
+        <li className="bio-list">Ethics in Computer Science</li>
+        <li className="bio-list">Automation</li>
+      </p>
+      <p>I speak Italian, English, beginner level of Norwegian and I'm studying Chinese (HSK3 level).</p>
+      <p>My hobbies are language learning and crafting, especially artistic journaling.</p>
     </>
   )
 }
